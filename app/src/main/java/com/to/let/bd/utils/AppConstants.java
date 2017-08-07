85cee51ed47d585e1b5145820c3c2b2fb6c77935
MAKINUL <mcnasim@gmail.com>
2017-08-08 00:08:30 +0600

    Complete post new Ad to server without image publish.

--- app/src/main/java/com/to/let/bd/utils/AppConstants.java
+++ app/src/main/java/com/to/let/bd/utils/AppConstants.java
@@ -1,12 +1,17 @@
 package com.to.let.bd.utils;
 
+import android.content.Context;
+import android.widget.EditText;
+
+import com.to.let.bd.R;
+
 public class AppConstants {
     public static final long textWatcherDelay = 2000;
 
 
     //-------------methods-----------
     // mobile number validation
-    public static boolean isMobileNumberValid(String mobileNumber) {
+    private static boolean isMobileNumberValid(String mobileNumber) {
         if (mobileNumber.length() < 11)
             return false;
         if (mobileNumber.startsWith("+88")) {
@@ -16,8 +21,28 @@ public class AppConstants {
             mobileNumber = mobileNumber.replace("88", "");
         }
 
-        return mobileNumber.length() >= 11 && (mobileNumber.startsWith("016") || mobileNumber.startsWith("017") || mobileNumber.startsWith("018") || mobileNumber.startsWith("019"));
+        return mobileNumber.length() == 11 && (mobileNumber.startsWith("016") ||
+                mobileNumber.startsWith("017") || mobileNumber.startsWith("018") ||
+                mobileNumber.startsWith("019"));
+    }
 
+    public static boolean isMobileNumberValid(Context context, EditText mobileNumber) {
+        if (mobileNumber == null)
+            return false;
+
+        if (mobileNumber.getText().length() == 0) {
+            mobileNumber.setError(context.getString(R.string.error_field_required));
+            mobileNumber.requestFocus();
+            return false;
+        }
+
+        if (!isMobileNumberValid(mobileNumber.getText().toString())) {
+            mobileNumber.setError(context.getString(R.string.error_valid_mobile_number));
+            mobileNumber.requestFocus();
+            return false;
+        }
+
+        return true;
     }
 
     private boolean mobileStartingValidation() {
